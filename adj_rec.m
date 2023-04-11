d404096f223d518cdcf6902bf52c12d45f88953e
= <=wesleygarlao@utexas.edu>
2023-04-11 11:47:01 -0500

    adj_rec

---
+++ adj_rec.m
@@ -0,0 +1,66 @@
+function [t,v,err] = adj_rec(Ns,Ks,times)
+    % solve adjoint ode and reconstruct to higher polynomial degree
+    Globals1D;
+    global y1 t1;
+    t = cell(Ks,1);
+    v = cell(Ks,1);
+    err = zeros(Ks,1);
+    vL_prev = 0;
+    y0 = 1;
+    % outer time step loop
+    for s = Ks:-1:1
+        tk = times(s:s+1);
+        fem_setup(Ns(s),1,tk);
+        hk = x(1) - x(end);
+        M = hk/2 .* inv(V*V'); 
+        S = inv(V*V')*Dr;
+        m = zeros(Np,Np); m(1) = -1;
+        A = -S'+m-M;
+        if s == Ks
+            F = M*ones(Np,1); F(end) = F(end) - vL_prev;
+        else
+            F = zeros(Np,1); F(end) = - vL_prev;
+        end
+%         F = M*ones(Np,1); F(end) = F(end) - vL_prev;
+%         F = M*(x.^2); F(end) = F(end) - vL_prev;
+
+%         Reconstruct with Radau Colocation Points
+        % Radau order m
+        rad_m = Ns(s) + 1;
+        rad = radau{rad_m};
+        rad_x = tk(1) + (1+rad).*abs(hk)./2;
+        
+        v_s = A\F;
+        % polyfit for interpolation
+        pa = polyfit(x,v_s,length(v_s)-1);
+        v_interp = @(t) polyval(pa,t);
+        v_rec = v_interp(rad_x); v_rec(end+1) = vL_prev;
+        
+        x_rec = [rad_x; tk(2)];
+        pa_rec = polyfit(x_rec,v_rec,rad_m);
+        vh_interp = @(t) polyval(pa_rec,t);
+                
+        fem_setup(rad_m,1,tk);
+        M = hk/2 .* inv(V*V'); 
+        S = inv(V*V')*Dr;
+        m = zeros(Np,Np); m(end) = 1;
+        A = -S'+m-M;
+        if s == 1
+            F = zeros(Np,1); F(1) = y0;
+        else
+            F = zeros(Np,1); F(1) = y1{s-1}(end);
+        end
+        
+        % polyfit for interpolation
+        pu = polyfit(t1{s},y1{s},length(y1{s})-1);
+        uh_interp = @(t) polyval(pu,t);
+        uh_s = uh_interp(x);
+        v_h = vh_interp(x);
+        err(s) = v_h'*((S'-m+M)*uh_s + F);
+                
+        v{s} = v_rec;
+        vL_prev = v_rec(1);
+        t{s} = x_rec;
+    end
+    return
+end
