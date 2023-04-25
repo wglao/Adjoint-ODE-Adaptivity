fcb735d69c75a70804d3d50a2ac2dacddd591f55
= <wesleygarlao@utexas.edu>
2023-04-25 10:10:53 -0500

    corrected animation

--- python/animate.py
+++ python/animate.py
@@ -2,12 +2,13 @@ import cv2
 import os
 
 
-def animate(case: str):
+def animate(case: str = 'test', framerate: int = 12):
   plots = os.listdir(case)
+  plots.sort(key=lambda x: os.path.getmtime(os.path.join(case,x)))
   frame = cv2.imread(os.path.join(case, plots[0]))
   height, width, _ = frame.shape
   video = cv2.VideoWriter(case + '/' + case + '.mp4',
-                          cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 12,
+                          cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), framerate,
                           (width, height))
   for i, p in enumerate(plots):
     p_path = os.path.join(case, p)
@@ -18,6 +19,7 @@ def animate(case: str):
   cv2.destroyAllWindows()
   video.release()
 
+
 if __name__ == "__main__":
   case = 'ResNet_no_matrix'
-  animate(case)
+  animate(case, 3)
